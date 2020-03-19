7c1cff8fd853a6952cb977a4c57280fbd92b4d0e
Soufiane MARZOUK <soufianemarzouk.2017@gmail.com>
2020-03-19 22:56:33 +0100

    Add dockerfiles

--- microservice-produits/src/main/java/com/mproduits/model/Product.java
+++ microservice-produits/src/main/java/com/mproduits/model/Product.java
@@ -1,21 +1,25 @@
 package com.mproduits.model;
 
 
+import org.hibernate.validator.constraints.Length;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
 @Entity
 public class Product {
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String titre;
 
     private String description;
 
+    @Length(max = 200000)
     private String image;
 
     private Double prix;
