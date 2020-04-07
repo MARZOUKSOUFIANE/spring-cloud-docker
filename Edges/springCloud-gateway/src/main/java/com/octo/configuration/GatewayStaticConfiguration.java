20069d625f931753d7be5fed72056396eaf1eedb
soufianeMARZOUK <soufianemarzouk.2017@gmail.com>
2020-04-07 16:27:52 +0100

    Add parent pom.xml

--- Edges/springCloud-gateway/src/main/java/com/octo/configuration/GatewayStaticConfiguration.java
+++ Edges/springCloud-gateway/src/main/java/com/octo/configuration/GatewayStaticConfiguration.java
@@ -1,5 +1,8 @@
 package com.octo.configuration;
 
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -10,15 +13,15 @@ public class GatewayStaticConfiguration {
        return builder.routes()
                 .route(r -> r.path("/produits/**").filters (f ->
                         f.hystrix(h->h.setName("Hystrix").setFallbackUri("forward:/produitsFallback")))
-                        .uri("http://localhost:9001/").id("r1"))
+                        .uri("http://produits:9001/").id("r1"))
 
                 .route(r -> r.path("/commandes/**").filters (f ->
                         f.hystrix(h->h.setName("Hystrix").setFallbackUri("forward:/commandesFallback")))
-                        .uri("http://localhost:9002/").id("r2"))
+                        .uri("http://commandes:9002/").id("r2"))
 
                 .route(r -> r.path("/paiements/**").filters (f ->
                         f.hystrix(h->h.setName("Hystrix").setFallbackUri("forward:/paiementFallback")))
-                        .uri("http://localhost:9003/").id("r3"))
+                        .uri("http://paiement:9003/").id("r3"))
 
                 .build();
     }*/
